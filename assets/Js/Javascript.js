afc3ec81e072700d909e575e33e42a2eb152c1e3
Denva <emsley3684@gmail.com>
2022-02-05 10:20:27 -0500

    added function to end game, check for answer, and show result

--- assets/Js/Javascript.js
+++ assets/Js/Javascript.js
@@ -113,58 +113,69 @@ const testQuestions = [
 
 ]
 
+let i = 9;
+let quiz = testQuestions[i];
+
 const timer = function() {
     let i = 101;    
-    var myTime = setInterval(function(){i--; timeEl.innerHTML= ""; $("#timer").append( "Time:  " + i );}, 1000)
-    setTimeout(function(){clearInterval(myTime)}, ((1000 * 10) * 10))    
+    var myTime = setInterval(function(){i--; timeEl.innerHTML= ""; $(timeEl).append( "Time:  " + i )}, 1000)
+    setTimeout(function(){clearInterval(myTime)}, ((1000 * 10) * 10))  
 }
 
-const buildQuiz = num => {
-    let i = num
-    const quiz = testQuestions[i];
-
+const buildQuiz = () => {
     var writeQuiz = () => {
         quizContainer.innerHTML = " "
 
         var questions = quiz.question 
         var answers = quiz.answers
-    
+         
         var quizQuest = $("<h3>").text(questions).add("</br>")
         $(quizContainer).append(quizQuest)
         for (x in answers){
             var newAns = $("<div>").text(answers[x])
-            var newBtn = $("<button>").text(x).attr('id', "answer").addClass("btn-dark");
-            $(newAns).append(newBtn)
+            var newBtn = $("<button>").text(x).attr("onclick", `subAnswer("${x}")`).addClass("btn btn-outline-dark");
+            $(newAns).prepend(newBtn)
             $(quizContainer).append(newAns)
-    
         }
-
-        const answerButton = document.querySelectorAll("#answer");
-
-        const subAnswer = () => {
-            console.log(x)
-            if(x == quiz.correctAnswer){
-                console.log("you picked correct")
-            }
-            else{
-                alert("wrong");
-            }
-    
-            i++ 
-            buildQuiz(i)
-        }
-        $(answerButton).click(subAnswer)
     }
     writeQuiz();
 } 
 
-const startQuiz = () => {
-     timer(); 
-     buildQuiz(0);
+const subAnswer = (clickedBtn) => {
+    if(clickedBtn == quiz.correctAnswer) {
+        i == i++
+        if( i == 10){
+            return showResults()
+        }
+        quiz = testQuestions[i]
+        return buildQuiz();
+    } else {
+        alert("You are incorrect");
+        return buildQuiz();
+    }
 }
 
-const showResults = () => {}
+const startQuiz = () => {
+     timer(); 
+     buildQuiz();
+}
 
+const showResults = () => {
+    let currentTime = timeEl.textContent;
+    const score = currentTime.split("").slice(7).join("");
+
+    const resultText = 
+    ` 
+    <div>
+        <h3> Your Score: ${score}</h3></br>
+
+        <p>Other High Scores: </p>
+        <ul class="high-scores"></ul>
+    </div>
+    `
+
+    quizContainer.innerHTML = resultText
+}
 
 startButton.addEventListener('click',startQuiz);
 submitButton.addEventListener('click', showResults);
